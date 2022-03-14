4cd783bf73720965354aa4f240c2148d2ba23c5b
Fausto M. Lagos S <fausto@autonomia.digital>
2022-03-14 16:24:51 -0500

    Setting up test framework

---
+++ main_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+type mainSuite struct {
+	suite.Suite
+}
+
+func TestMainSuite(t *testing.T) {
+	suite.Run(t, new(mainSuite))
+}
+
+func (s *mainSuite) Test_AlwaysSucceeds() {
+	s.Equal(1, 1)
+}
