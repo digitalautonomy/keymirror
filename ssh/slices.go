d817cade82f5b448345625c34e4b5517169184eb
ivanjijon <ivanjijon@autonomia.digital>
2022-03-15 16:57:31 -0500

    Adding generic helper functions for slice manipulation

--- ssh/slices.go
+++ ssh/slices.go
@@ -1,9 +1,58 @@
 package ssh
 
+import "golang.org/x/exp/constraints"
+
 func transform[T, U any](l []T, f func(T) U) []U {
 	output := []U{}
 	for _, l := range l {
 		output = append(output, f(l))
 	}
 	return output
+}
+
+func isEmptySlice[T any](v []T) bool {
+	return len(v) == 0
+}
+
+func isEmpty[T comparable](v T) bool {
+	var zero T
+
+	// IDEA does not handle this correctly yet. But Golang 1.18 does
+	return zero == v
+}
+
+func isGreaterThan[T constraints.Ordered](cutoff T) predicate[T] {
+	return func(v T) bool {
+		return v > cutoff
+	}
+}
+
+func isEqualTo[T comparable](expected T) predicate[T] {
+	return func(v T) bool {
+		return expected == v
+	}
+}
+
+func isNil[T any](v *T) bool {
+	return v == nil
+}
+
+type func1[T, U any] func(T) U
+type predicate[T any] func1[T, bool]
+
+func not[T any](f predicate[T]) predicate[T] {
+	return func(v T) bool {
+		return !f(v)
+	}
+}
+
+func filter[T any](v []T, f predicate[T]) []T {
+	res := []T{}
+
+	for _, t := range v {
+		if f(t) {
+			res = append(res, t)
+		}
+	}
+	return res
 }
---
+++ ssh/slices_test.go
@@ -0,0 +1,71 @@
+package ssh
+
+import (
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+type genericsSuite struct {
+	suite.Suite
+}
+
+func TestGenericsSuite(t *testing.T) {
+	suite.Run(t, new(genericsSuite))
+}
+
+func (s *genericsSuite) Test_isEmpty() {
+	s.True(isEmpty(false))
+	s.False(isEmpty(true))
+
+	s.True(isEmpty(0))
+	s.False(isEmpty(1))
+
+	s.True(isEmpty(""))
+	s.False(isEmpty("fo"))
+}
+
+func (s *genericsSuite) Test_isEmptySlice() {
+	s.True(isEmptySlice([]string{}))
+	s.False(isEmptySlice([]string{""}))
+}
+
+func (s *genericsSuite) Test_isNil() {
+	var t1 *string
+	var t2 *int
+	var t3 *string = new(string)
+	var t4 *int = new(int)
+
+	s.True(isNil(t1))
+	s.True(isNil(t2))
+	s.False(isNil(t3))
+	s.False(isNil(t4))
+}
+
+func (s *genericsSuite) Test_not() {
+	f1 := func(s string) bool {
+		return s == "foo" || s == "bar"
+	}
+
+	s.True(f1("foo"))
+	s.True(f1("bar"))
+	s.False(f1("quux"))
+
+	inv := not(f1)
+	s.False(inv("foo"))
+	s.False(inv("bar"))
+	s.True(inv("quux"))
+
+	f2 := func(s int) bool {
+		return s == 42
+	}
+	inv2 := not(f2)
+	s.False(inv2(42))
+}
+
+func (s *genericsSuite) Test_filter() {
+	l := []string{"a", "", "b"}
+	s.Equal([]string{""}, filter(l, isEmpty[string]))
+	s.Equal([]string{"a", "b"}, filter(l, not(isEmpty[string])))
+	s.Equal([]int{43, 43}, filter([]int{41, 42, 43, 44, 43}, isEqualTo(43)))
+	s.Equal([]int{43, 44, 43}, filter([]int{41, 42, 43, 44, 43}, isGreaterThan(42)))
+}
